8c0e11f4fd11cd403a121be2cc1754fee4057988
Anton Aksiuta <anton.aksiuta@gallagher.com>
2017-08-23 09:24:01 +1200

    IU-2017.2.2 <aaksiuta@GNZ-PC1561 Create markdown.xml, IntelliLang.xml, project.default.xml, diff.xml, equalsHashCodeTemplates.xml, Enum.java, Interface.java, applicationLibraries.xml, Default.xml, file.template.settings.xml, find.xml, File Header.java, gradle.run.settings.xml, code.style.schemes.xml, git.xml, mavenVersion.xml, Default.xml, GoogleStyle _1_.xml, scala.xml, laf.xml, debugger.renderers.xml, duplocatorSettings.xml, editor.codeinsight.xml, editor.xml, hg.xml, debugger.xml, path.macros.xml, baseRefactoring.xml, ide.general.xml, other.xml, GoogleStyle.xml, xpath.xml, keymap.xml, ui.lnf.xml, filetypes.xml, package-info.java, javaeeExternalResources.xml, plugin_ui.xml, colors.scheme.xml, External Tools.xml, notifications.xml, databaseDrivers.xml, AnnotationType.java, Project Default.xml, parameter.hints.xml, Class.java, jdk.table.xml, web-browsers.xml, vcs.xml, databaseSettings.xml, toStringTemplates.xml, usageView.xml, sonarlint.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,10 @@
+//
+//         Copyright Gallagher Group Ltd ${YEAR} All Rights Reserved
+//             THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF
+//                Gallagher Group Research and Development
+//                          Hamilton, New Zealand
+//
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+public class ${NAME} {
+}
---
+++ fileTemplates/internal/package-info.java
@@ -0,0 +1,8 @@
+//
+//         Copyright Gallagher Group Ltd ${YEAR} All Rights Reserved
+//             THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF
+//                Gallagher Group Research and Development
+//                          Hamilton, New Zealand
+//
+#parse("File Header.java")
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
